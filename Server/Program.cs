b8d6b4bdb20a33bd7afe2f49023652c6868e96ea
sebastianjb <bryant.sebastianj@gmail.com>
2023-05-19 14:48:06 -0500

    Working on grocery list functionality

--- Server/Program.cs
+++ Server/Program.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.EntityFrameworkCore;
 
-namespace GreenPantryApp.Server
+namespace GreenPantryApp
 {
     public class Program
     {
