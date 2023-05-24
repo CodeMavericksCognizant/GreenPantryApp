ca0981a2cd15b5481662301cbb521839e2e05be2
sebastianjb <bryant.sebastianj@gmail.com>
2023-05-24 13:59:07 -0500

    adding razor pages

--- Server/Models/GroceryItem.cs
+++ Server/Models/GroceryItem.cs
@@ -1,7 +1,10 @@
-﻿namespace GreenPantryApp.Server.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GreenPantryApp.Server.Models
 {
     public class GroceryItem
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
         public int Id { get; set; }
         public string Name { get; set; }
         public DateTime ExpiryDate { get; set; }
